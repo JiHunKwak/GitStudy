2d1288575345ccde4c92772c05860a4b24969ea5
Kwak Jihun <rhkrwlgns160@gmail.com>
2017-05-08 00:32:41 +0900

    5/7 Last commit

--- Prac.java
+++ Prac.java
@@ -6,6 +6,8 @@ public class Prac {
 		
 		System.out.println("Hello Git!");
 		System.out.println("Second Commit!");
+		System.out.println("Third Commit!");
+		System.out.println("test line");
 	}
 
 }
